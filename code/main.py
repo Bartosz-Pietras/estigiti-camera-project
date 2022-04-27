f2bf39280f8dee2ccf5ef19a09bfe9154b91089d
Dominik Soltysiak <soltysiakdominiks@gmail.com>
2022-04-27 20:28:53 +0200

    Added evaluation of results and changed dimensions of model's layers

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import matplotlib.pyplot as plt
 from torchvision import datasets, transforms, models
 from torch.utils.data.sampler import SubsetRandomSampler
 
@@ -10,14 +11,14 @@ from model import Model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-transform = transforms.Compose([transforms.Resize(224),
+transform = transforms.Compose([transforms.Resize((298, 224)),
                                 transforms.ToTensor(),
                                 ])
 
 # Adding a random comment just to see if GitHub issues and pull requests work properly.
 # Setting the hyperparameters
-num_epochs = 5
-batch_size = 16
+num_epochs = 20
+batch_size = 1
 learning_rate = 0.001
 
 # Loading and splitting the data into train/test set
@@ -45,12 +46,15 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Training loop
 n_total_steps = len(train_loader)
+accuracy = 0
+running_loss, test_loss = 0, 0
+train_losses, test_losses = [], []
+
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         # origin shape: [4, 3, 32, 32] = 4, 3, 1024
         # input_layer: 3 input channels, 6 output channels, 5 kernel size
-        images = images.to(device)
-        labels = labels.to(device)
+        images, labels = images.to(device), labels.to(device)
 
         # Forward pass
         outputs = model(images)
@@ -60,14 +64,38 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
 
-        if (i + 1) % 2000 == 0:
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+        if (i + 1) % 10 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.3f}')
+    train_losses.append(running_loss / len(train_loader))
+    running_loss = 0
 
-print('Finished Training')
+print('Finished the training')
 
 # Save the model
 PATH = '../model/cnn.pth'
 torch.save(model.state_dict(), PATH)
 
-# Visualize the loss and results
+# Evaluate the validation loss and accuracy
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        results = model(images)
+        batch_loss = criterion(results, labels)
+        test_loss += batch_loss.item()
+
+        ps = torch.exp(results)
+        top_p, top_class = ps.topk(1, dim=1)
+        equals = top_class == labels.view(*top_class.shape)
+        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+    test_losses.append(test_loss / len(test_loader))
+    print(f"Test loss: {test_loss / len(test_loader):.3f}\n"
+          f"Test accuracy: {accuracy / len(test_loader):.3f}")
+
+# Plot of the training and validation losses
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
+plt.show()
--- code/model.py
+++ code/model.py
@@ -8,15 +8,15 @@ class Model(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(60208, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 5)
 
     def forward(self, x):
         x = self.pool(functional.relu(self.conv1(x)))  # -> n, 6, 14, 14
         x = self.pool(functional.relu(self.conv2(x)))  # -> n, 16, 5, 5
-        x = x.view(-1, 16 * 5 * 5)  # -> n, 400
+        x = x.view(-1, 60208)  # -> n, 400
         x = functional.relu(self.fc1(x))  # -> n, 120
         x = functional.relu(self.fc2(x))  # -> n, 84
-        x = self.fc3(x)  # -> n, 10
+        x = self.fc3(x)  # -> num of classes (5)
         return x
