b7230651088b18e4df8c92b9ce8ac217800b066e
yuriboyka14 <podkowinka14@gmail.com>
2022-05-22 17:54:53 +0200

    utils.py minor change - loading existing model + dummy_input added

--- code/utils.py
+++ code/utils.py
@@ -2,17 +2,21 @@ import torch.onnx
 from model import Model
 
 # Function to Convert to ONNX
-def Convert_to_ONNX(path):
+def Convert_to_ONNX(path_to_converted):
 
     model = Model()
+    path = "model/leaves_recogniser.pth"
+    model.load_state_dict(torch.load(path))
 
     # set the model to inference mode
     model.eval()
 
+    dummy_input = torch.randn(1, 3, requires_grad=True)   # 3 randomowo wybralem - to jest po prostu array (?) 1x3
+
     # Export the model
     torch.onnx.export(model,  # model being run
-                      None,  # model input (or a tuple for multiple inputs) - na razie dalem None bo nie wiem do konca
-                      path,  # where to save the model
+                      dummy_input,  # model input (or a tuple for multiple inputs) - na razie dalem None bo nie wiem do konca
+                      path_to_converted,  # where to save the model
                       export_params=True,  # store the trained parameter weights inside the model file
                       opset_version=10,  # the ONNX version to export the model to
                       do_constant_folding=True,  # whether to execute constant folding for optimization
