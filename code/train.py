48161db0b444b8592b1b4a58fe2fbe0aaecbe131
Dominik Soltysiak <soltysiakdominiks@gmail.com>
2022-06-04 23:57:12 +0200

    Added confusion matrix, changed to 4 classes(stage 4+5 merged), moved model evaluation part of the code to utils.py

--- code/train.py
+++ code/train.py
@@ -1,15 +1,15 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
+from utils import evaluate_model, select_proper_device
 from model import Model
 
 # Device configuration
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+device = select_proper_device()
+print(f"Chosen device is: {device}")
 
 transform = transforms.Compose([transforms.Resize((298, 224)),
                                 transforms.RandomRotation(degrees=(-90, -90)),
@@ -18,13 +18,12 @@ transform = transforms.Compose([transforms.Resize((298, 224)),
 
 # Setting the hyperparameters
 HYPERPARAMETERS = {
-    "epochs": 300,
-    "batch_size": 5,
-    "learning_rate": 1e-3,
+    "epochs": 35,
+    "batch_size": 32,
+    "learning_rate": 1e-2,
     "train_size": 0.7,
     "valid_size": 0.2,
 }
-
 
 # Loading and splitting the data into train/test set
 bean_dataset = datasets.ImageFolder("../dataset/", transform=transform)
@@ -38,7 +37,6 @@ train_dataset, valid_dataset, test_dataset = torch.utils.data.random_split(
     bean_dataset, (train_count, valid_count, test_count)
 )
 
-
 train_loader = torch.utils.data.DataLoader(
     train_dataset, batch_size=HYPERPARAMETERS["batch_size"], shuffle=True
 )
@@ -51,14 +49,13 @@ test_loader = torch.utils.data.DataLoader(
 
 model = Model().to(device)
 
-criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=HYPERPARAMETERS["learning_rate"])
+criterion = nn.CrossEntropyLoss()
+
+validation_loss = 0.0
+train_losses, validation_losses = np.array([]), np.array([])
 
 # Training loop
-n_total_steps = len(train_loader)
-accuracy = 0
-train_losses, validation_losses, test_losses = np.array([]), np.array([]), np.array([])
-
 for epoch in range(HYPERPARAMETERS["epochs"]):
     training_loss = 0.0
     for i, (images, labels) in enumerate(train_loader):
@@ -82,7 +79,7 @@ for epoch in range(HYPERPARAMETERS["epochs"]):
             )
 
     model.eval()
-    validation_loss = 0.0
+
     for images, labels in valid_loader:
         images, labels = images.to(device), labels.to(device)
         output = model(images)
@@ -99,36 +96,10 @@ PATH = "../model/bean_stage_recogniser_01_06_2022.pth"
 torch.save(model.state_dict(), PATH)
 
 # Evaluate the validation loss and accuracy
-with torch.no_grad():
-    test_loss = 0.0
-    for images, labels in test_loader:
-        images, labels = images.to(device), labels.to(device)
-        results = model(images)
-        batch_loss = criterion(results, labels)
-        test_loss += batch_loss.item()
-
-        ps = torch.exp(results)
-        top_p, top_class = ps.topk(1, dim=1)
-        equals = top_class == labels.view(*top_class.shape)
-        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-
-        test_losses = np.append(test_losses, test_loss)
-    print(
-        f"Test loss: {test_loss / len(test_loader):.3f}\n"
-        f"Test accuracy: {accuracy / len(test_loader):.3f}"
-    )
-
-
-# Plot of the training and validation losses
 epochs = np.arange(HYPERPARAMETERS["epochs"]) + 1
+train_size = len(train_loader)
+valid_size = len(valid_loader)
 
-fig, ax = plt.subplots()
-
-ax.set_xticks(epochs)
-ax.set_xlabel("Epochs")
-ax.set_ylabel("Loss value")
-ax.plot(epochs, train_losses/len(train_loader), label="Training loss")
-ax.plot(epochs, validation_losses/len(valid_loader), label="Validation loss")
-ax.legend(frameon=False)
-ax.grid()
-plt.show()
+evaluate_model(
+    model, test_loader, epochs, train_size, valid_size, train_losses, validation_losses
+)
--- code/utils.py
+++ code/utils.py
@@ -1,12 +1,19 @@
 import torch.onnx
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
 from model import Model
+
 
 # Function to Convert to ONNX
 def convert_to_ONNX():
     torch_model_path = "../model/leaves_recogniser.pth"
     onnx_model_path = "../model/leaves_recogniser.onnx"
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = select_proper_device()
 
     model = Model()
     if torch.cuda.is_available():
@@ -34,6 +41,77 @@ def convert_to_ONNX():
     )
 
     print("\nModel has been converted to ONNX")
+
+
+def evaluate_model(model, test_loader, epochs, train_size, valid_size, train_losses, validation_losses):
+    criterion = nn.CrossEntropyLoss()
+    device = select_proper_device()
+    cf_matrix = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    accuracy, test_loss = 0, 0
+    test_losses = np.array([])
+
+    # Evaluate the validation loss and accuracy
+    with torch.no_grad():
+        test_loss = 0.0
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            results = model(images)
+            batch_loss = criterion(results, labels)
+            test_loss += batch_loss.item()
+
+            ps = torch.exp(results)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+            # Reducing the dimensionality of the tensor
+            top_class_cm = torch.squeeze(
+                top_class, 1
+            )
+            cf_matrix_batch = confusion_matrix(
+                labels.cpu().numpy(), top_class_cm.cpu().numpy()
+            )
+            cf_matrix = np.add(cf_matrix, cf_matrix_batch)
+
+            test_losses = np.append(test_losses, test_loss)
+
+        print(
+            f"Test loss: {test_loss / len(test_loader):.3f}\n"
+            f"Test accuracy: {accuracy / len(test_loader):.3f}"
+        )
+
+    plot_confusion_matrix(cf_matrix)
+    plot_losses(epochs, train_losses, validation_losses, train_size, valid_size)
+
+
+def plot_confusion_matrix(cf_matrix):
+    ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues')
+
+    ax.set_title('Seaborn Confusion Matrix with labels\n');
+    ax.set_xlabel('Predicted stage')
+    ax.set_ylabel('Actual stage\n');
+
+    # Ticket labels - List must be in alphabetical order
+    ax.xaxis.set_ticklabels(['Stage 1', 'Stage 2', 'Stage 3', 'Stage 4'])
+    ax.yaxis.set_ticklabels(['Stage 1', 'Stage 2', 'Stage 3', 'Stage 4'])
+
+    plt.show()
+
+
+def plot_losses(epochs, train_losses, validation_losses, train_size, valid_size):
+    fig, ax = plt.subplots()
+    ax.set_xticks(epochs)
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Loss value")
+    ax.plot(epochs, train_losses / train_size, label="Training loss")
+    ax.plot(epochs, validation_losses / valid_size, label="Validation loss")
+    ax.legend(frameon=False)
+    ax.grid()
+    plt.show()
+
+
+def select_proper_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 if __name__ == "__main__":
